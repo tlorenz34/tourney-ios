5161ebcdc6e20364f025a493a9fbd17d766b5f1a
german espitia <ge.espitia@gmail.com>
2019-11-06 12:52:42 -0500

    Clean up code for Login View Controller

--- LoginVC.swift
+++ LoginVC.swift
@@ -15,68 +15,12 @@ class LoginVC: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var loginEmailField: UITextField!
     @IBOutlet weak var loginPasswordField: UITextField!
     @IBOutlet weak var doesNotExistLabel: UILabel!
-    
     @IBOutlet weak var roundedLoginButton: UIButton!
-    
-    
+
     var userUid: String!
+
+    // MARK: - Actions
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        loginEmailField.delegate = self
-        loginPasswordField.delegate = self
-        self.roundedLoginButton.layer.cornerRadius = 7
-        
-        // Do any additional setup after loading the view.
-    }
-    
-
-    override func viewDidAppear(_ animated: Bool) {
-        if let _ = KeychainWrapper.standard.string(forKey: "uid") {
-            User.sharedInstance.uid = KeychainWrapper.standard.string(forKey: "uid")
-            DatabaseService.loadSingletonData { (success) in
-                self.goToFeedVC()
-            }
-        }
-    }
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
-    }
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        
-        return (true)
-    }
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // used to be "toSignUp"
-        if segue.identifier == "toSignUp"{
-            
-            if let destination = segue.destination as? UserVC{
-                if userUid != nil{
-                    destination.userUid = userUid
-                }
-                if loginEmailField.text != nil {
-                    destination.emailField = loginEmailField.text
-                }
-                if loginPasswordField.text != nil {
-                    destination.passwordField = loginPasswordField.text
-                }
-                
-            }
-            
-        }
-    }
-
-    func goToCreateUserVC(){
-        performSegue(withIdentifier: "toSignUp", sender: nil)
-    }
- 
-    func goToFeedVC(){
-        performSegue(withIdentifier: "toFeedVC", sender: nil)
-        
-    }
- 
     @IBAction func loginInTapped(_ sender: Any){
         if let email = loginEmailField.text, let password = loginPasswordField.text {
             Auth.auth().signIn(withEmail: email, password: password, completion:
@@ -101,6 +45,66 @@ class LoginVC: UIViewController, UITextFieldDelegate {
         }
     }
     
+    // MARK: - View lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loginEmailField.delegate = self
+        loginPasswordField.delegate = self
+        self.roundedLoginButton.layer.cornerRadius = 7
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if let _ = KeychainWrapper.standard.string(forKey: "uid") {
+            User.sharedInstance.uid = KeychainWrapper.standard.string(forKey: "uid")
+            DatabaseService.loadSingletonData { (success) in
+                self.goToFeedVC()
+            }
+        }
+    }
+
+    // MARK: - Helpers
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // used to be "toSignUp"
+        if segue.identifier == "toSignUp"{
+            
+            if let destination = segue.destination as? UserVC{
+                if userUid != nil{
+                    destination.userUid = userUid
+                }
+                if loginEmailField.text != nil {
+                    destination.emailField = loginEmailField.text
+                }
+                if loginPasswordField.text != nil {
+                    destination.passwordField = loginPasswordField.text
+                }
+                
+            }
+            
+        }
+    }
+    
+    func goToCreateUserVC(){
+        performSegue(withIdentifier: "toSignUp", sender: nil)
+    }
+
+    func goToFeedVC(){
+        performSegue(withIdentifier: "toFeedVC", sender: nil)
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return (true)
+    }
+
     
 }
 
