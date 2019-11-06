86ac98fc8790d1bbf5460d9c90f4c5c3d39a2e0e
german espitia <ge.espitia@gmail.com>
2019-11-06 15:17:55 -0500

    Update competitions table view to correctly display cells

--- Tourney/Competition View/TableViewController.swift
+++ Tourney/Competition View/TableViewController.swift
@@ -24,7 +24,8 @@ class TableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        data = [CellData(image: UIImage(named: "BMX_Competition_1"), message: "", filter: "BMX_Competition_1"), CellData(image: UIImage(named: "BMX_Competition_2"), message: "", filter: "BMX_Competition_2")]
+        data = [CellData(image: UIImage(named: "BMX_Competition_1"), message: "BMX #1", filter: "BMX_Competition_1"),
+                CellData(image: UIImage(named: "BMX_Competition_2"), message: "BMX #2", filter: "BMX_Competition_2")]
         self.tableView.register(CustomCell.self, forCellReuseIdentifier: "custom")
         self.tableView.rowHeight = UITableView.automaticDimension
         // used to be 200
@@ -34,13 +35,16 @@ class TableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return data.count
     }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 400
+    }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
      
         let cell = self.tableView.dequeueReusableCell(withIdentifier: "custom") as! CustomCell
         cell.mainImage = data[indexPath.row].image
-        cell.message = data[indexPath.row].message
-        cell.layoutSubviews()
+        cell.titleLabel.text = data[indexPath.row].message
         return cell
         
     }
