770c2e7e13b4334423830dc0580a521fefdde838
german espitia <ge.espitia@gmail.com>
2019-12-19 13:55:44 -0500

    Add LoadingButton class to signify loading within buttons.

---
+++ Tourney/Views/LoadingButton.swift
@@ -0,0 +1,62 @@
+//
+//  LoadingUIButton.swift
+//  Tourney
+//
+//  Created by German Espitia on 12/19/19.
+//  Copyright © 2019 Messenger. All rights reserved.
+//
+
+import UIKit
+
+class LoadingUIButton: UIButton {
+
+    @IBInspectable var indicatorColor : UIColor = .white
+
+    var originalButtonText: String?
+    var activityIndicator: UIActivityIndicatorView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    func showLoading() {
+        originalButtonText = self.titleLabel?.text
+        self.setTitle("", for: .normal)
+
+        if (activityIndicator == nil) {
+            activityIndicator = createActivityIndicator()
+        }
+
+        showSpinning()
+    }
+
+    func hideLoading() {
+        DispatchQueue.main.async(execute: {
+            self.setTitle(self.originalButtonText, for: .normal)
+            self.activityIndicator.stopAnimating()
+        })
+    }
+
+    private func createActivityIndicator() -> UIActivityIndicatorView {
+        let activityIndicator = UIActivityIndicatorView()
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.color = indicatorColor
+        return activityIndicator
+    }
+
+    private func showSpinning() {
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(activityIndicator)
+        centerActivityIndicatorInButton()
+        activityIndicator.startAnimating()
+    }
+
+    private func centerActivityIndicatorInButton() {
+        let xCenterConstraint = NSLayoutConstraint(item: self, attribute: .centerX, relatedBy: .equal, toItem: activityIndicator, attribute: .centerX, multiplier: 1, constant: 0)
+        self.addConstraint(xCenterConstraint)
+
+        let yCenterConstraint = NSLayoutConstraint(item: self, attribute: .centerY, relatedBy: .equal, toItem: activityIndicator, attribute: .centerY, multiplier: 1, constant: 0)
+        self.addConstraint(yCenterConstraint)
+    }
+
+}
