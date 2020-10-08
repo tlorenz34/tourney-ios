38127933b1a932fbe82b52eea74ff1e450ebf591
german <ge.espitia@gmail.com>
2020-10-08 14:11:39 -0500

    Add parsing and loading of Tournament objects

--- Tourney/Managers/TournamentManager.swift
+++ Tourney/Managers/TournamentManager.swift
@@ -7,13 +7,50 @@
 //
 
 import Foundation
-
+import Firebase
 /**
  Interface for `Tournament` between database and client.
  */
 struct TournamentManager {
     
+    let db = Firestore.firestore()
+    private let tournamentsCollectionKey = "tournaments"
+    private var baseQuery: Query {
+        return db.collection(tournamentsCollectionKey)
+    }
+    /**
+     Fetch `Tournament` objects with `active` property as `true`
+     */
     func fetchActiveTournaments(completion: @escaping (([Tournament]?) -> Void)) {
+        baseQuery.whereField("active", isEqualTo: true).getDocuments { (snapshot, error) in
+            guard let snapshot = snapshot else {
+                completion(nil)
+                return
+            }
+            completion(self.parseDocumentsToTournaments(documents: snapshot.documents))
+        }
         completion(nil)
+    }
+    /**
+     Parse array of `QueryDocumentSnapshot` objects to array of `Tournament` objects
+     */
+    private func parseDocumentsToTournaments(documents: [QueryDocumentSnapshot]) -> [Tournament] {
+        var tournaments: [Tournament] = []
+        for document in documents {
+            if let tournament = parseDocumentToTournament(document: document) {
+                tournaments.append(tournament)
+            }
+        }
+        return tournaments
+    }
+    /**
+     Parse `QueryDocumentSnapshot` to `Tournament`
+     */
+    private func parseDocumentToTournament(document: QueryDocumentSnapshot) -> Tournament? {
+        if let tournament = Tournament(id: document.documentID, dictionary: document.data()) {
+            return tournament
+        } else {
+            return nil
+        }
     }
 }
--- Tourney/Models/Tournament.swift
+++ Tourney/Models/Tournament.swift
@@ -11,4 +11,36 @@ import Foundation
 struct Tournament {
     let name: String
     let id: String
+    let featuredImageURL: URL
+    let participants: Int
+    /// Leader info (may be nil due to no leader due to no votes)
+    let leaderId: String?
+    let leaderUsername: String?
+    let leaderProfileImageURL: URL?
+    
+    init?(id: String, dictionary: [String : Any]) {
+        guard let name = dictionary["name"] as? String,
+              let participants = dictionary["participants"] as? Int,
+              let featuredImageURLString = dictionary["featuredImageURL"] as? String,
+              let featuredImageURL = URL(string: featuredImageURLString) else {
+            return nil
+        }
+        self.id = id
+        self.name = name
+        self.participants = participants
+        self.featuredImageURL = featuredImageURL
+        
+        if let leaderId = dictionary["leaderId"] as? String,
+           let leaderUsername = dictionary["leaderUsername"] as? String,
+           let leaderProfileImageURLString = dictionary["leaderProfileImageURL"] as? String,
+           let leaderProfileImageURL = URL(string: leaderProfileImageURLString) {
+            self.leaderId = leaderId
+            self.leaderUsername = leaderUsername
+            self.leaderProfileImageURL = leaderProfileImageURL
+        } else {
+            self.leaderId = nil
+            self.leaderUsername = nil
+            self.leaderProfileImageURL = nil
+        }
+    }
 }
