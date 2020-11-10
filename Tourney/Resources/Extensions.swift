27845556c70b39c9317096b0d6eacb1817b7856f
German Espitia <german@lindytech.io>
2020-11-10 18:25:42 -0500

    Add extension to show UIAlertController from any UIViewController

--- Tourney/Resources/Extensions.swift
+++ Tourney/Resources/Extensions.swift
@@ -7,9 +7,20 @@
 //
 
 import Foundation
+import UIKit
 
 extension Collection where Indices.Iterator.Element == Index {
     subscript (exist index: Index) -> Iterator.Element? {
         return indices.contains(index) ? self[index] : nil
+    }
+}
+extension UIViewController {
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let dismissAction = UIAlertAction(title: "Ok", style: .default) { (_) in
+            alert.dismiss(animated: true, completion: nil)
+        }
+        alert.addAction(dismissAction)
+        present(alert, animated: true, completion: nil)
     }
 }
